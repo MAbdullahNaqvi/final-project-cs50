8d5b234ff0408ff8a1e78a0d8292aeaa73d429f6
MAbdullahNaqvi <abdullahhhussainnaqvi@gmail.com>
2023-08-27 23:12:46 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, render_template, redirect,jsonify,request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("layout.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
