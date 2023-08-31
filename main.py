982d3954c7edfed7d3bcfab6c56e4fb070f322cd
MAbdullahNaqvi <abdullahhussainnaqvi@gmail.com>
2023-08-31 16:43:43 -0700

    main error checks

--- main.py
+++ main.py
@@ -4,9 +4,25 @@ from flask import Flask, render_template, redirect,jsonify,request
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/",methods=["GET", "POST"])
 def home():
-    return render_template("home.html")
+    
+    name = request.form.get("name")
+    code = request.form.get("room-id")
+    create = request.form.get("create")
+    join = request.form.get("join", False)
+
+    if request.method == "POST":
+        if not request.form.get("name"):
+            error = "Name Not Provided!!"
+            return render_template("home.html", name = name, code = code, error = error)
+        if join != False and not request.form.get("room_id"):
+            error = "Room Code Not Provided!!"
+            return render_template("home.html", name = name, code = code, error = error)
+        
+    else:
+        return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
